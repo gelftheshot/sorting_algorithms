26a5cf74615903f62992fbbfbbc45202df28da37
Gelfeto Gebre <gelf.learing@gmail.com>
2023-10-18 23:07:11 +0300

    my first commit of the day

---
+++ README.md
@@ -0,0 +1,1 @@
+this is the sorting algoriths readme file 
---
+++ sort.h
@@ -0,0 +1,6 @@
+#ifndef _SORT_H
+#define _SORT_H
+
+void insertion_sort_list(listint_t **list);
+void bubble_sort(int *array, size_t size);
+
