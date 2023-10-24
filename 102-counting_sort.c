bd5d38cf554254d9d362492f285ec0b514f60123
Gelfeto Gebre <gelf.learing@gmail.com>
2023-10-24 20:06:20 +0300

    fixing ede case

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -12,29 +12,47 @@ void counting_sort(int *array, size_t size)
 	int n = size;
 	int i, j, max = 0;
 	int *count_array;
-	int *temp = malloc(sizeof(int) * size);
+	int *temp;
 	size_t count_len;
 
+	if (array == NULL || size < 2)
+		return;
+
+	
 	for (i = 0; i < n; i++)
 	{
 		if (array[i] > max)
 			max = array[i];
 	}
 	count_array = malloc(sizeof(int) * (max + 1));
+	if (count_array == NULL)
+	{
+		return;
+	}
+	temp = malloc(sizeof(int) * size);
+	if (temp == NULL)
+	{
+		free(count_array);
+		return;
+	}
+
 	count_len = max + 1;
 
 	for (i = 0; i < n; i++)
 	{
 		temp[i] = array[i];
 	}
-	for (j = 0; j < max; j++)
+
+	for (j = 0; j <= max; j++)
 	{
 		count_array[j] = 0;
 	}
+
 	for (i = 0; i < n; i++)
 	{
 		count_array[array[i]] = count_array[array[i]] + 1;
 	}
+
 	for (i = 1; i <= max; i++)
 	{
 		count_array[i] = count_array[i] + count_array[i - 1];
