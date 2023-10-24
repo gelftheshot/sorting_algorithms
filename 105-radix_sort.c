bcca14841d8a31c80dba8f48f578bf93244f5bce
Gelfeto Gebre <gelf.learing@gmail.com>
2023-10-25 01:20:07 +0300

    radix sort

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,15 +1,18 @@
 #include "sort.h"
 
 /**
- *
- *
+ *digit_count_sort - counting sort
+ *@array: the array to be sorted
+ *@size: the size of the array
+ *@digit: which digit is being sorted
+ *Return: void
  */
 
 void digit_count_sort(int *array, int size, int digit)
 {
 
 	int n = size;
-	int i, j,m, max = 0;
+	int i, j, m, max = 0;
 
 	int *count_array;
 	int *temp = malloc(sizeof(int) * size);
@@ -19,7 +22,7 @@ void digit_count_sort(int *array, int size, int digit)
 		if (((array[i] / digit) % 10)  >  max)
 			max = (array[i] / digit) % 10;
 	}
-	
+
 	count_array = malloc(sizeof(int) * (max + 1));
 
 
@@ -38,17 +41,22 @@ void digit_count_sort(int *array, int size, int digit)
 		m = (array[i] / digit) % 10;
 		count_array[m] = count_array[m] + 1;
 	}
-    for (i = 1; i <= max; i++)
-        count_array[i] = count_array[i] + count_array[i - 1];
-    for (i = n - 1; i >= 0; i--)
- 
-    {
-    	array[count_array[(temp[i] / digit) % 10] - 1] = temp[i];
-    	count_array[(temp[i] / digit ) % 10]--;
-    }
+	for (i = 1; i <= max; i++)
+		count_array[i] = count_array[i] + count_array[i - 1];
+	for (i = n - 1; i >= 0; i--)
 
-	
+	{
+		array[count_array[(temp[i] / digit) % 10] - 1] = temp[i];
+		count_array[(temp[i] / digit) % 10]--;
+	}
 }
+
+/**
+ *radix_sort - the real radx sort
+ *@array: the array
+ *@size: the size
+ *Return: void
+**/
 
 void radix_sort(int *array, size_t size)
 {
@@ -63,9 +71,10 @@ void radix_sort(int *array, size_t size)
 			max = array[i];
 	}
 
-	for (digit = 1; max/digit > 0; digit = digit * 10)
+	for (digit = 1; max / digit > 0; digit = digit * 10)
 	{
 		digit_count_sort(array, size, digit);
+		print_array(array, size);
 	}
 
 }
